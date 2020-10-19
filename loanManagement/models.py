041b183f5273000db229e31ba4784a1e6b56ef2c
sakshishreyaa <sakshishreyaaaaa@gmail.com>
2020-10-19 17:25:45 +0530

    Authentication using jwt

--- loanManagement/models.py
+++ loanManagement/models.py
@@ -30,7 +30,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True, default=3)
     date_joined = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=True)
     is_deleted = models.BooleanField(default=False)
     created_date = models.DateTimeField(default=timezone.now)
     modified_date = models.DateTimeField(default=timezone.now)
