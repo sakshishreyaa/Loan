9ecda7aa26ab16edb8d19f467a7ca67ac0cb27d0
Sakshi <i.sakshi.shreya@redcarpetup.com>
2021-02-06 15:52:22 +0530

    herok

--- loan/settings.py
+++ loan/settings.py
@@ -177,11 +177,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_URL = '/static/'
